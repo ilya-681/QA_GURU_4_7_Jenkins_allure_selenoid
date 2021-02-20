1e67f10ff41e94a6b7eae78f0cff20e2eb2998e8
ilya-681 <ilya-681@ya.ru>
2021-02-21 00:22:43 +0300

    Added allure info and hamcrest

--- src/test/java/simpleTests.java
+++ src/test/java/simpleTests.java
@@ -1,5 +1,8 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class simpleTests {
@@ -11,5 +14,17 @@ public class simpleTests {
     @Test
     void negativeTest() {
         assertEquals(true, false);
+    }
+
+    @Test
+    @DisplayName("Positive hamcrest test")
+    void successHamcrestTest() {
+        assertThat(true, is(true));
+    }
+
+    @Test
+    @DisplayName("Negative hamcrest test")
+    void negativeHamcrestTest() {
+        assertThat(true, is(false));
     }
 }
